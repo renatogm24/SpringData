2dd3a7a9433b2ee1b7e9c22ea0be703aa33acae1
renatogm24 <renato.garay@pucp.pe>
2022-02-17 12:09:02 -0500

    Add contoller and view show book

---
+++ SpringData/src/main/java/com/codingdojo/springdata/controllers/BookController.java
@@ -0,0 +1,55 @@
+package com.codingdojo.springdata.controllers;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.codingdojo.springdata.models.Book;
+import com.codingdojo.springdata.services.BookService;
+
+@Controller
+public class BookController {
+	private final BookService bookService;
+
+	public BookController(BookService bookService){
+	        this.bookService = bookService;
+	    }
+
+	@RequestMapping("/books")
+	public List<Book> index() {
+		List<Book> list = bookService.allBooks();
+		return list;
+	}
+
+	@RequestMapping(value = "/books", method = RequestMethod.POST)
+	public Book create(@RequestParam(value = "title") String title, @RequestParam(value = "description") String desc,
+			@RequestParam(value = "language") String lang, @RequestParam(value = "pages") Integer numOfPages) {
+		Book book = new Book(title, desc, lang, numOfPages);
+		return bookService.createBook(book);
+	}
+
+	@RequestMapping("/books/{id}")
+	public String show(Model model, @PathVariable("id") Long id) {
+		Book book = bookService.findBook(id);
+		model.addAttribute("book", book);
+		return "show.jsp";
+	}
+
+	@RequestMapping(value = "/books/{id}", method = RequestMethod.PUT)
+	public Book update(@PathVariable("id") Long id, @RequestParam(value = "title") String title,
+			@RequestParam(value = "description") String desc, @RequestParam(value = "language") String lang,
+			@RequestParam(value = "pages") Integer numOfPages) {
+		Book book = new Book(id, title, desc, lang, numOfPages);
+		return bookService.updateBook(book);
+	}
+
+	@RequestMapping(value = "/books/{id}", method = RequestMethod.DELETE)
+	public void destroy(@PathVariable("id") Long id) {
+		bookService.deleteBook(id);
+	}
+}
